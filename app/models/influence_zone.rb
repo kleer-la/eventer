ed6378a026cd96c82f809a450262f30b951de6f2
Juan Gabardini <jgabardini@computer.org>
2016-01-16 08:58:13 -0300

    fix error: unknown country in influence zone raise an exception in display name

--- app/models/influence_zone.rb
+++ app/models/influence_zone.rb
@@ -1,12 +1,16 @@
 class InfluenceZone < ActiveRecord::Base
   belongs_to :country
   default_scope order('country_id, zone_name ASC')
-  
+
   attr_accessible :tag_name, :zone_name, :country_id, :country
-  
+
   validates :tag_name, :country, :presence => true
-  
+
   def display_name
-    self.zone_name == "" ? self.country.name : self.country.name+" - "+self.zone_name
+    if self.country.nil?
+      self.zone_name
+    else
+      self.zone_name == "" ? self.country.name : self.country.name+" - "+self.zone_name
+    end
   end
 end
