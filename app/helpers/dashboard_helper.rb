c6add21ab7cae98206123f20cd1f98275f95ba8e
Juan Gabardini <jgabardini@computer.org>
2016-02-16 16:17:36 -0300

    handle pricing index when event have no EB

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,5 @@
 module DashboardHelper
-  
+
   def currency_symbol_for( iso_code )
     currency = Money::Currency.table[iso_code.downcase.to_sym] unless iso_code.nil?
     if currency.nil?
@@ -8,5 +8,9 @@ module DashboardHelper
       currency[:symbol]
     end
   end
-  
+
+  def short_date(d)
+    d.nil? ? '' : d.to_formatted_s(:short)
+  end
+
 end
