a71fa7b7032efc71e948ae484b1c8137680a827e
Juan Gabardini <jgabardini@computer.org>
2016-02-21 14:53:42 -0300

    message when the attendance list is empty

--- spec/views/participants/print.html.haml_spec.rb
+++ spec/views/participants/print.html.haml_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe "rendering the attendance list template" do
-  it "displays the participants" do
-  participant= FactoryGirl.create(:participant)
-  assign(:participants, [participant])
-  assign(:event, participant.event)
+  it "displays the participant list" do
+    participant= FactoryGirl.create(:participant)
+    assign(:participants, [participant])
+    assign(:event, participant.event)
 
-  render :template => "participants/print"
+    render :template => "participants/print"
 
-  expect(rendered).to match /Juan Carlos/
-end
+    expect(rendered).to match /Juan Carlos/
+  end
+  it "displays a message for an empty participant list" do
+    assign(:participants, [])
+    assign(:event, FactoryGirl.create(:event))
+
+    render :template => "participants/print"
+
+    expect(rendered).to match /No hay participantes/
+  end
 end
