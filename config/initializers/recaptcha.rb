635a41d96cbcda35cc331800230895d81b001535
yamit <yamit.cardenas@gmail.com>
2017-05-23 14:41:54 -0500

    removed comments

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -3,5 +3,3 @@ Recaptcha.configure do |config|
   config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
 
-#export RECAPTCHA_SITE_KEY="6Ld_iSIUAAAAAC2VRB5HPMSoEvQb4UGREvqbxGbQ"
-#export RECAPTCHA_SECRET_KEY="6Ld_iSIUAAAAAClJtl3YV7HfT4B4idylgMrBLQ9k"
