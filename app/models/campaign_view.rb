66b75cf7d1a5dd39586d2b6050d95b8c9c445780
Martín Alaimo <malaimo@gmail.com>
2017-07-28 14:26:13 -0300

    Changed View Touch After commit

--- app/models/campaign_view.rb
+++ app/models/campaign_view.rb
@@ -5,7 +5,7 @@ class CampaignView < ActiveRecord::Base
 
   attr_accessible :campaign, :campaign_source, :event, :element_viewed
 
-  after_create do |view|
+  after_commit do |view|
     view.campaign.touch unless view.campaign.nil?
     view.campaign_source.touch unless view.campaign_source.nil?
   end
