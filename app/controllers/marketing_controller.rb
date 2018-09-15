054673bb000dc10957d4ad3093bb7e5d23b58f95
Martín Alaimo <malaimo@gmail.com>
2018-09-15 11:34:09 -0500

    fix on marketing home campaign retrival

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -44,7 +44,7 @@ class MarketingController < ApplicationController
       else
         @since = DateTime.now-36000
       end
-      @camapign_ids = CampaignView.where("updated_at >= ?", @since ).where("updated_at < ?", @until ).map{ |p| p.campaign_id }
+      @camapign_ids = CampaignView.where("created_at >= ?", @since ).where("created_at < ?", @until ).map{ |p| p.campaign_id }
       @camapigns = Campaign.where('id in (?)', @camapign_ids ).order('updated_at DESC')
     end
   end
