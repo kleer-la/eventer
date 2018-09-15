f2965f6875c01682c00683590c89e0becf38c61a
Martín Alaimo <malaimo@gmail.com>
2018-09-15 12:35:57 -0500

    Temporary Removing After Commit CallBack on CampaignView

--- app/models/campaign_view.rb
+++ app/models/campaign_view.rb
@@ -6,8 +6,8 @@ class CampaignView < ActiveRecord::Base
 
   attr_accessible :campaign, :campaign_source, :event, :element_viewed
 
-  after_commit do |view|
-    view.campaign.touch unless view.campaign.nil?
-    view.campaign_source.touch unless view.campaign_source.nil?
-  end
+  # after_commit do |view|
+  #   view.campaign.touch unless view.campaign.nil?
+  #   view.campaign_source.touch unless view.campaign_source.nil?
+  # end
 end
