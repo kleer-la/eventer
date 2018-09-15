4ec2d497a465ef551fa7dc65ba4866b59512aeb0
Martín Alaimo <malaimo@gmail.com>
2018-09-15 12:32:05 -0500

    Demanding Migration

--- db/migrate/20180915161333_add_event_type_to_campaign_view.rb
+++ db/migrate/20180915161333_add_event_type_to_campaign_view.rb
@@ -3,10 +3,10 @@ class AddEventTypeToCampaignView < ActiveRecord::Migration
     add_column :campaign_views, :event_type_id, :integer, index: true
     add_index :campaign_views, :event_type_id
 
-    CampaignView.reset_column_information
-
-    CampaignView.all.each do |cv|
-      cv.update_attribute(:event_type, cv.event.event_type) if !cv.event.nil?
-    end
+    # CampaignView.reset_column_information
+    #
+    # CampaignView.all.each do |cv|
+    #   cv.update_attribute(:event_type, cv.event.event_type) if !cv.event.nil?
+    # end
   end
 end
