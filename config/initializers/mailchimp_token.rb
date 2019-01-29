80161db02e09dd6ded69b2ae07ee5ca88d21d3be
Hiro <hhiroshi@Hiros-MacBook-Pro.local>
2019-01-29 13:05:46 -0500

    changing mailchimp temporal key

--- config/initializers/mailchimp_token.rb
+++ config/initializers/mailchimp_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Eventer::Application.config.mailchimp_token = 'c0b5889988a71bb22d8c1f9423fb19c1-us1'
+Eventer::Application.config.mailchimp_token = 'f08f62a253d9dc63476e0b679b825f43-us1'
