2eab7b2669de74623b5366fbca624725101787f6
Juan Gabardini <jgabardini@computer.org>
2020-12-19 18:23:24 -0300

    last... moving should to expect

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,26 +9,11 @@ describe User do
       @admin = Ability.new ( FactoryGirl.create(:administrator) )
     end
   
-    it "should be able to manage roles" do
-      @admin.should be_able_to(:manage, Role.new)
-    end
-    
-    it "should be able to manage users" do
-      @admin.should be_able_to(:manage, User.new)
-    end
-    
-    it "should be able to manage events" do
-      @admin.should be_able_to(:manage, Event.new)
-    end
-    
-     it "should be able to manage trainers" do
-        @admin.should be_able_to(:manage, Trainer.new)
-     end
-  
-     it "should be able to manage trainers" do
-        @admin.should be_able_to(:manage, Category.new)
-     end
-     
+    it { expect(@admin).to be_able_to(:manage, Role.new)}
+    it { expect(@admin).to be_able_to(:manage, User.new)}
+    it { expect(@admin).to be_able_to(:manage, Event.new)}
+    it { expect(@admin).to be_able_to(:manage, Trainer.new)}
+    it { expect(@admin).to be_able_to(:manage, Category.new)}    
   end
   
   context "If it's a comercial person" do
@@ -37,31 +22,12 @@ describe User do
       @comercial = Ability.new ( FactoryGirl.create(:comercial) )
     end
 
-  
-    it "shouldn't be able to manage roles" do
-      @comercial.should_not be_able_to(:manage, Role.new)
-    end
-    
-    it "shouldn't be able to manage users" do
-      @comercial.should_not be_able_to(:manage, User.new)      
-    end
-    
-    it "should be able to manage events" do
-      @comercial.should be_able_to(:manage, Event.new)
-    end
-    
-    it "shouldn't be able to manage users" do
-      @comercial.should_not be_able_to(:manage, EventType.new)      
-    end
-    
-    it "shouldn't be able to manage users" do
-      @comercial.should_not be_able_to(:manage, Category.new)      
-    end
-    
-    it "should be able to manage trainers" do
-      @comercial.should_not be_able_to(:manage, Trainer.new)
-    end
-  
+    it { expect(@comercial).not_to be_able_to(:manage, Role.new)}
+    it { expect(@comercial).not_to be_able_to(:manage, User.new)}
+    it { expect(@comercial).to be_able_to(:manage, Event.new)}
+    it { expect(@comercial).not_to be_able_to(:manage, EventType.new)}
+    it { expect(@comercial).not_to be_able_to(:manage, Category.new)}
+    it { expect(@comercial).not_to be_able_to(:manage, Trainer.new)}  
   end
 
 end
