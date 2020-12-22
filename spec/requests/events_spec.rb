18acce2b1b385291b2199506f37c612d834e9a8f
Juan Gabardini <jgabardini@computer.org>
2020-12-22 17:11:28 -0300

    from cucumber to rspec: API one event

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -8,11 +8,19 @@ describe "Events" do
       Rack::Builder.parse_file("config.ru").first
     end
     
-    it "event list" do
-      event = FactoryGirl.build(:event)      
+    it "event list in XML" do
       event_url= '/api/events.xml'
       get event_url
       expect(last_response.status).to eq 200
+      expect(last_response.body).to start_with("<?xml")
+    end
+
+    it "event list public and visible courses in XML" do
+      event = FactoryGirl.create(:event)      
+      event_url= '/api/events.xml'
+      get event_url
+      parsed= Nokogiri::XML(last_response.body)
+      expect(parsed.xpath('//event').count).to eq 1
     end
   end
 end
