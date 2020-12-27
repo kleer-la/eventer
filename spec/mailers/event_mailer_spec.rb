80ce38bb772b7416580d08a016c2236cc23a5bd0
Juan Gabardini <jgabardini@computer.org>
2020-12-27 16:39:49 -0300

    recover test for prices custom text in mails

--- spec/mailers/event_mailer_spec.rb
+++ spec/mailers/event_mailer_spec.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 require "spec_helper"
+
 describe EventMailer do
     before :each do
         @participant = FactoryBot.build(:participant)
@@ -12,11 +13,30 @@ describe EventMailer do
     it "welcome_new_webinar_participant", :pending => "Deprecated - should remove" do fail;end
     it "notify_webinar_start" , :pending => "Deprecated - should remove"  do fail;end
     it "alert_event_crm_push_finished" , :pending => "Deprecated - should remove" do fail;end
+    #   context 'alert_event_crm_push_finished' do
+    #     it 'dont send notification when PushTransation dont have email address' do
+    #       email= EventMailer.alert_event_crm_push_finished(CrmPushTransaction.new)
+    #       expect(email.body).to eq ''
+    #     end
+    
+    #     it 'send registration in event with alert email address' do
+    #       cpt= CrmPushTransaction.new
+    #       cpt.user= FactoryBot.build(:user)
+    #       email= EventMailer.alert_event_crm_push_finished(cpt)
+    
+    #       expect(email.subject).to include('CRM finalizado')
+    #       expect(email.body).to include('finalizado')
+    #     end
+    #   end 
     
     context 'welcome_new_event_participant' do
+        EB_TEXT="Pronto pago:"
+        PAIR_TEXT="En parejas:"
+        GROUP_TEXT="Grupos (5 o más):"
+
         it "should queue and verify a simple email" do
             @email = EventMailer.welcome_new_event_participant(@participant).deliver
-            File.open("x.html", 'w') { |file| file.write(@email) }
+            # File.open("x.html", 'w') { |file| file.write(@email) }
             expect(ActionMailer::Base.deliveries.count).to be 1
             expect(@email.to).to eq ["app_test@kleer.la"]
             expect(@email.subject).to eq "Kleer | Concurso de truco"
@@ -35,111 +55,93 @@ describe EventMailer do
         it "should not send early bird prices if EB info is empty" do
             email = EventMailer.welcome_new_event_participant(@participant).deliver
             
-            expect(email.text_part.body.to_s).not_to include "Pronto pago:"
-            expect(email.html_part.body.to_s).not_to include "Pronto pago:"
+            expect(email.text_part.body.to_s).not_to include EB_TEXT
+            expect(email.html_part.body.to_s).not_to include EB_TEXT
         end
         
-          it "should send early bird prices if EB info is available" do
+        it "should send early bird prices if EB info is available" do
             @participant.event.eb_end_date = Date.today
             @participant.event.eb_price = 180
-        
+            
             email = EventMailer.welcome_new_event_participant(@participant).deliver
-        
-            expect(email.text_part.body.to_s).to include "Pronto pago:"
-            expect(email.html_part.body.to_s).to include "Pronto pago:"
+            
+            expect(email.text_part.body.to_s).to include EB_TEXT
+            expect(email.html_part.body.to_s).to include EB_TEXT
             expect(email.text_part.body.to_s).to include "ARS $ 180"
             expect(email.html_part.body.to_s).to include "ARS $ 180"
         end
         
+        it "should not show 2 person price if NOT present" do
+            email = EventMailer.welcome_new_event_participant(@participant).deliver
+            
+            expect(email.text_part.body.to_s).not_to include PAIR_TEXT
+            expect(email.html_part.body.to_s).not_to include PAIR_TEXT
+        end
         it "should show 2 person price if present" do
             @participant.event.couples_eb_price = 950
             
             email = EventMailer.welcome_new_event_participant(@participant).deliver
             
-            expect(email.text_part.body.to_s).to include "En parejas:"
-            expect(email.html_part.body.to_s).to include "En parejas:"
+            expect(email.text_part.body.to_s).to include PAIR_TEXT
+            expect(email.html_part.body.to_s).to include PAIR_TEXT
             expect(email.text_part.body.to_s).to include "ARS $ 950"
             expect(email.html_part.body.to_s).to include "ARS $ 950"
+        end
+
+        it "should not show group price if NOT present" do
+            email = EventMailer.welcome_new_event_participant(@participant).deliver
+            
+            expect(email.text_part.body.to_s).not_to include GROUP_TEXT
+            expect(email.html_part.body.to_s).not_to include GROUP_TEXT
+        end
+        it "should show group price if present" do
+            @participant.event.business_eb_price = 850
+    
+            email = EventMailer.welcome_new_event_participant(@participant).deliver
+    
+            expect(email.text_part.body.to_s).to include GROUP_TEXT
+            expect(email.html_part.body.to_s).to include GROUP_TEXT
+            expect(email.text_part.body.to_s).to include "ARS $ 850 cada uno abonando antes del"
+            expect(email.html_part.body.to_s).to include "ARS $ 850 cada uno abonando antes del"
+        end
+        
+        it "should replace all prices if custom text is present" do
+            @participant.event.list_price = 200
+            @participant.event.eb_end_date = Date.today
+            @participant.event.eb_price = 180
+            @participant.event.couples_eb_price = 100
+            @participant.event.business_eb_price = 150
+
+            @participant.event.custom_prices_email_text = "texto customizado"
+
+            email = EventMailer.welcome_new_event_participant(@participant).deliver
+
+            expect(email.text_part.body.to_s).not_to include EB_TEXT
+            expect(email.html_part.body.to_s).not_to include EB_TEXT
+            expect(email.text_part.body.to_s).not_to include PAIR_TEXT
+            expect(email.html_part.body.to_s).not_to include PAIR_TEXT
+            expect(email.text_part.body.to_s).not_to include GROUP_TEXT
+            expect(email.html_part.body.to_s).not_to include GROUP_TEXT
+            expect(email.text_part.body.to_s).not_to include "ARS $ 200"
+            expect(email.html_part.body.to_s).not_to include "ARS $ 200"
+            expect(email.text_part.body.to_s).not_to include "ARS $ 180"
+            expect(email.html_part.body.to_s).not_to include "ARS $ 180"
+            expect(email.text_part.body.to_s).not_to include "ARS $ 100"
+            expect(email.html_part.body.to_s).not_to include "ARS $ 100"
+            expect(email.text_part.body.to_s).not_to include "ARS $ 150"
+            expect(email.html_part.body.to_s).not_to include "ARS $ 150"
+
+            expect(email.text_part.body.to_s).to include "texto customizado"
+            expect(email.html_part.body.to_s).to include "texto customizado"
+        end
+        
+        it "should send the custom text in HTML format if custom text markdown is present" do
+            @participant.event.custom_prices_email_text = "**texto customizado**: 16"
+            
+            email = EventMailer.welcome_new_event_participant(@participant).deliver
+            
+            expect(email.html_part.body.to_s).to include "<strong>texto customizado</strong>: 16"
           end
-        
-        #   it "should show 5 person price if present" do
-        #     @participant.event.business_eb_price = 850
-        
-        #     email = EventMailer.welcome_new_event_participant(@participant).deliver
-        
-        #     text_message = email.body.parts.find {|p| p.content_type.match /plain/}.body.raw_source
-        #     text_message.should include("850.00 pagando de a 5 personas antes del")
-        
-        #     html_message = email.body.parts.find {|p| p.content_type.match /html/}.body.raw_source
-        #     html_message.should include("850.00 pagando de a 5 personas antes del")
-        #   end
-        
-        #   it "should not send list price or early bird prices if custom text is present" do
-        #     @participant.event.list_price = 200
-        #     @participant.event.eb_end_date = Date.today
-        #     @participant.event.eb_price = 180
-        #     @participant.event.couples_eb_price = 100
-        #     @participant.event.business_eb_price = 150
-        
-        #     @participant.event.custom_prices_email_text = "texto customizado"
-        
-        #     email = EventMailer.welcome_new_event_participant(@participant).deliver
-        
-        #     text_message = email.body.parts.find {|p| p.content_type.match /plain/}.body.raw_source
-        #     text_message.should_not include("(ARS) $ 200.00")
-        #     text_message.should_not include("(ARS) $ 180.00")
-        #     text_message.should_not include("pagando de a 2 personas")
-        #     text_message.should_not include("pagando de a 5 personas")
-        
-        #     html_message = email.body.parts.find {|p| p.content_type.match /html/}.body.raw_source
-        #     html_message.should_not include("(ARS) $ 200.00")
-        #     html_message.should_not include("(ARS) $ 180.00")
-        #     html_message.should_not include("Si pagas antes del")
-        #     html_message.should_not include("pagando de a 2 personas")
-        #     html_message.should_not include("pagando de a 5 personas")
-        #   end
-        
-        #   it "should send the custom text if custom text is present" do
-        #     @participant.event.custom_prices_email_text = "texto customizado"
-        
-        #     email = EventMailer.welcome_new_event_participant(@participant).deliver
-        
-        #     text_message = email.body.parts.find {|p| p.content_type.match /plain/}.body.raw_source
-        #     text_message.should include("texto customizado")
-        #     expect(text_message).not_to include("Promociones no acumulables")
-        
-        #     html_message = email.body.parts.find {|p| p.content_type.match /html/}.body.raw_source
-        #     html_message.should include("texto customizado")
-        #     expect(html_message).not_to include("Promociones no acumulables")
-        #   end
-        
-        #   it "should send the custom text in HTML format if custom text markdown is present" do
-        #     @participant.event.custom_prices_email_text = "**texto customizado**: 16"
-        
-        #     email = EventMailer.welcome_new_event_participant(@participant).deliver
-        
-        #     html_message = email.body.parts.find {|p| p.content_type.match /html/}.body.raw_source
-        #     html_message.should include("<strong>texto customizado</strong>: 16")
-        
-        #   end
-        #   context 'alert_event_crm_push_finished' do
-        #     it 'dont send notification when PushTransation dont have email address' do
-        #       email= EventMailer.alert_event_crm_push_finished(CrmPushTransaction.new)
-        #       expect(email.body).to eq ''
-        #     end
-        
-        #     it 'send registration in event with alert email address' do
-        #       cpt= CrmPushTransaction.new
-        #       cpt.user= FactoryBot.build(:user)
-        #       email= EventMailer.alert_event_crm_push_finished(cpt)
-        
-        #       expect(email.subject).to include('CRM finalizado')
-        #       expect(email.body).to include('finalizado')
-        #     end
-        #   end
-        
-        
-        # end
     end
 
     it "should send the certificate e-mail" do
@@ -176,6 +178,3 @@ describe EventMailer do
     end
 
 end
-
-
-
