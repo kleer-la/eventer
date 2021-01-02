08984a9267fc8b8a76933aebe58a8fa3db1129c3
Juan Gabardini <jgabardini@computer.org>
2021-01-02 15:19:30 -0300

    migrate spec/views/roles

---
+++ spec/views/roles/index.html.haml_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "roles/index" do
+  before(:each) do
+    assign(:roles, [
+      FactoryBot.create(:admin_role, :name => "R Name 1"),
+      FactoryBot.create(:comercial_role, :name => "R Name 2")
+    ])
+  end
+
+  it "renders a list of roles" do
+    render
+        
+    expect(rendered).to match(/R Name 1/)
+    expect(rendered).to match(/R Name 2/)
+end
+end
