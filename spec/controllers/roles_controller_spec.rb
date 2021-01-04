f9179314620657e742ee40ebb7651ab11e0b1cc6
Juan Gabardini <jgabardini@computer.org>
2021-01-04 15:47:08 -0300

    migrating role controller tests (10/20)

--- spec/controllers/roles_controller_spec.rb
+++ spec/controllers/roles_controller_spec.rb
@@ -1,112 +1,88 @@
 require 'rails_helper'
+require_relative "../support/devise"
 
-
-
-# describe RolesController do
+describe RolesController do 
   
+  context "If user is administrator" do
+    login_admin
 
-#   # This should return the minimal set of attributes required to create a valid
-#   # Role. As you add validations to Role, be sure to
-#   # update the return value of this method accordingly.
-#   def valid_attributes
-#     { :name => "a-role-name" }
-#   end
-  
-#   # This should return the minimal set of values that should be in the session
-#   # in order to pass any filters (e.g. authentication) defined in
-#   # RolesController. Be sure to keep this updated too.
-#   def valid_session
-#     nil
-#   end
-  
-#   context "If user is administrator" do
-    
-#     before(:each) do
-#       @request.env["devise.mapping"] = Devise.mappings[:user]
-#       @user = FactoryBot.create(:administrator)
-#       sign_in @user
-#     end
+    before(:each) do
+      @role= FactoryBot.create(:comercial_role)
+    end
+    describe "GET index" do
+      it "assigns all roles as @roles" do
+        get :index, {}
+        expect(assigns(:roles).map(&:name)).to match_array ["administrator","comercial"]
+      end
+    end
 
-#     describe "GET index" do
-#       it "assigns all roles as @roles" do
-#         role = Role.create! valid_attributes
-#         roles_all = Role.all
-#         get :index, {}, valid_session
-#         assigns(:roles).should eq(roles_all)
-#       end
-#     end
+    describe "GET show" do
+      it "assigns the requested role as @role" do
+        get :show, {:id => @role.to_param}
+        expect(assigns(:role)).to eq @role
+      end
+    end
 
-#     describe "GET show" do
-#       it "assigns the requested role as @role" do
-#         role = Role.create! valid_attributes
-#         get :show, {:id => role.to_param}, valid_session
-#         assigns(:role).should eq(role)
-#       end
-#     end
+    describe "GET new" do
+      it "assigns a new role as @role" do
+        get :new, {}
+        expect(assigns(:role)).to be_a_new Role
+      end
+    end
 
-#     describe "GET new" do
-#       it "assigns a new role as @role" do
-#         get :new, {}, valid_session
-#         assigns(:role).should be_a_new(Role)
-#       end
-#     end
+    describe "GET edit" do
+      it "assigns the requested role as @role" do
+        get :edit, {:id => @role.to_param}
+        expect(assigns(:role)).to eq @role
+      end
+    end
 
-#     describe "GET edit" do
-#       it "assigns the requested role as @role" do
-#         role = Role.create! valid_attributes
-#         get :edit, {:id => role.to_param}, valid_session
-#         assigns(:role).should eq(role)
-#       end
-#     end
+    describe "POST create" do
+      describe "with valid params" do
+        it "creates a new Role" do
+          expect {
+            post :create, {:role => @role.attributes}
+          }.to change(Role, :count).by(1)
+        end
 
-#     describe "POST create" do
-#       describe "with valid params" do
-#         it "creates a new Role" do
-#           expect {
-#             post :create, {:role => valid_attributes}, valid_session
-#           }.to change(Role, :count).by(1)
-#         end
+        it "assigns a newly created role as @role" do
+          post :create, {:role => @role.attributes}
+          expect(assigns(:role)).to be_a Role
+          expect(assigns(:role)).to be_persisted
+        end
 
-#         it "assigns a newly created role as @role" do
-#           post :create, {:role => valid_attributes}, valid_session
-#           assigns(:role).should be_a(Role)
-#           assigns(:role).should be_persisted
-#         end
+        it "redirects to the created role" do
+          post :create, {:role => @role.attributes}
+          expect(response).to redirect_to(Role.last)
+        end
+      end
 
-#         it "redirects to the created role" do
-#           post :create, {:role => valid_attributes}, valid_session
-#           response.should redirect_to(Role.last)
-#         end
-#       end
+      describe "with invalid params" do
+        before(:each) do 
+          # Trigger the behavior that occurs when invalid params are submitted
+          allow_any_instance_of(Role).to receive(:save).and_return(false)
+          post :create, {:role => {}}
+        end
+        it "assigns a newly created but unsaved role as @role" do
+          expect(assigns(:role)).to be_a_new Role
+        end
 
-#       describe "with invalid params" do
-#         it "assigns a newly created but unsaved role as @role" do
-#           # Trigger the behavior that occurs when invalid params are submitted
-#           Role.any_instance.stub(:save).and_return(false)
-#           post :create, {:role => {}}, valid_session
-#           assigns(:role).should be_a_new(Role)
-#         end
+        it "re-renders the 'new' template" do
+          expect(response).to render_template("new")
+        end
+      end
+    end
 
-#         it "re-renders the 'new' template" do
-#           # Trigger the behavior that occurs when invalid params are submitted
-#           Role.any_instance.stub(:save).and_return(false)
-#           post :create, {:role => {}}, valid_session
-#           response.should render_template("new")
-#         end
-#       end
-#     end
-
-#     describe "PUT update" do
-#       describe "with valid params" do
-#         it "updates the requested role" do
-#           role = Role.create! valid_attributes
-#           # Assuming there are no other roles in the database, this
-#           # specifies that the Role created on the previous line
-#           # receives the :update_attributes message with whatever params are
-#           # submitted in the request.
-#           Role.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-#           put :update, {:id => role.to_param, :role => {'these' => 'params'}}, valid_session
-#         end
+    describe "PUT update" do
+      describe "with valid params" do
+        it "updates the requested role" do
+          # Assuming there are no other roles in the database, this
+          # specifies that the Role created on the previous line
+          # receives the :update_attributes message with whatever params are
+          # submitted in the request.
+          expect_any_instance_of(Role).to receive(:update_attributes).with({'these' => 'params'})
+          put :update, {:id => @role.to_param, :role => {'these' => 'params'}}
+        end
 
 #         it "assigns the requested role as @role" do
 #           role = Role.create! valid_attributes
@@ -119,7 +95,7 @@ require 'rails_helper'
 #           put :update, {:id => role.to_param, :role => valid_attributes}, valid_session
 #           response.should redirect_to(role)
 #         end
-#       end
+      end
 
 #       describe "with invalid params" do
 #         it "assigns the role as @role" do
@@ -138,7 +114,7 @@ require 'rails_helper'
 #           response.should render_template("edit")
 #         end
 #       end
-#     end
+    end
 
 #     describe "DELETE destroy" do
 #       it "destroys the requested role" do
@@ -189,6 +165,6 @@ require 'rails_helper'
 #     #  end
 #     #end
 
-#   end
+  end
 
-# end
+end
