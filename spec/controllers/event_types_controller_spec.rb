117a20ef9b497874c5e5491537031f69cdbfe385
Juan Gabardini <jgabardini@computer.org>
2021-01-04 10:32:26 -0300

    migrating participant controller tests (15/25)

--- spec/controllers/event_types_controller_spec.rb
+++ spec/controllers/event_types_controller_spec.rb
@@ -100,7 +100,7 @@ describe EventTypesController do
       before(:each) do 
         @event_types = FactoryBot.create :event_type
         allow_any_instance_of(EventType).to receive(:save).and_return(false)
-        put :update, {:id => @event_types.to_param, :evemt => {}}
+        put :update, {:id => @event_types.to_param, :event => {}}
       end
       it "assigns the event_type as @event_type" do
         expect(assigns(:event)).to eq @event_type
