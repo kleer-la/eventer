b18448a25a85c08a57f5e868822377aa37a8f0b7
Juan Gabardini <jgabardini@computer.org>
2021-01-05 16:42:40 -0300

    more test for draft on api

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,20 +4,37 @@ describe HomeController do
 
   describe "GET 'index' (/api/events.<format>)" do
     it "returns http success" do
-      get 'index'
+      get :index
       expect(response).to be_success
     end
     it "returns events" do
       event = FactoryBot.create(:event)
-      get 'index'
+      get :index
       expect(assigns(:events)).to eq [event]
     end
     it "returns non draft events" do
       event = FactoryBot.create(:event, place: "here")
       draft = FactoryBot.create(:event, place: "there", draft: true)
-      get 'index'
+      get :index
       expect(assigns(:events).map(&:place)).to eq ["here"]
     end
   end
 
+  describe "GET 'show' (/api/events.<format>)" do
+    it "fetch a course" do
+      event = FactoryBot.create(:event)
+      get :show, {:id => event.to_param}
+      expect(assigns(:event)).to eq event
+    end
+    it "illegal course  not found" do
+      expect{
+        get :show, {:id => 1}
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+    it "draft course found" do
+      event = FactoryBot.create(:event, draft: false)
+      get :show, {:id => event.to_param}
+      expect(assigns(:event)).to eq event
+    end
+  end
 end
