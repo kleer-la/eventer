34a28310f2fdc510049278576cea6620340e7498
Juan Gabardini <jgabardini@computer.org>
2021-01-05 15:31:11 -0300

    don't show draft event in public course list

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
-# describe HomeController do
+describe HomeController do
 
-#   describe "GET 'index'" do
-#     it "returns http success" do
-#       get 'index'
-#       response.should be_success
-#     end
-#   end
+  describe "GET 'index' (/api/events.<format>)" do
+    it "returns http success" do
+      get 'index'
+      expect(response).to be_success
+    end
+    it "returns events" do
+      event = FactoryBot.create(:event)
+      get 'index'
+      expect(assigns(:events)).to eq [event]
+    end
+    it "returns non draft events" do
+      event = FactoryBot.create(:event, place: "here")
+      draft = FactoryBot.create(:event, place: "there", draft: true)
+      get 'index'
+      expect(assigns(:events).map(&:place)).to eq ["here"]
+    end
+  end
 
-# end
+end
