14311eab294b1a3f42c4ff4f9dd56999327b9da8
Juan Gabardini <jgabardini@computer.org>
2021-01-05 17:53:11 -0300

    restore all routing tests

--- spec/routing/events_routing_spec.rb
+++ spec/routing/events_routing_spec.rb
@@ -1,35 +1,35 @@
-require "spec_helper"
+require 'rails_helper'
 
-# describe EventsController do
-#   describe "routing" do
+describe EventsController do
+  describe "routing" do
 
-#     it "routes to #index" do
-#       get("/events").should route_to("events#index")
-#     end
+    it "routes to #index" do
+      expect(get("/events")).to route_to("events#index")
+    end
 
-#     it "routes to #new" do
-#       get("/events/new").should route_to("events#new")
-#     end
+    it "routes to #new" do
+      expect(get("/events/new")).to route_to("events#new")
+    end
 
-#     it "routes to #show" do
-#       get("/events/1").should route_to("events#show", :id => "1")
-#     end
+    it "routes to #show" do
+      expect(get("/events/1")).to route_to("events#show", :id => "1")
+    end
 
-#     it "routes to #edit" do
-#       get("/events/1/edit").should route_to("events#edit", :id => "1")
-#     end
+    it "routes to #edit" do
+      expect(get("/events/1/edit")).to route_to("events#edit", :id => "1")
+    end
 
-#     it "routes to #create" do
-#       post("/events").should route_to("events#create")
-#     end
+    it "routes to #create" do
+      expect(post("/events")).to route_to("events#create")
+    end
 
-#     it "routes to #update" do
-#       put("/events/1").should route_to("events#update", :id => "1")
-#     end
+    it "routes to #update" do
+      expect(put("/events/1")).to route_to("events#update", :id => "1")
+    end
 
-#     it "routes to #destroy" do
-#       delete("/events/1").should route_to("events#destroy", :id => "1")
-#     end
+    it "routes to #destroy" do
+      expect(delete("/events/1")).to route_to("events#destroy", :id => "1")
+    end
 
-#   end
-# end
+  end
+end
