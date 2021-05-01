2abbdf543e021c9f58c227b7613425812d99655c
Juan Gabardini <jgabardini@computer.org>
2021-05-01 12:19:02 +0000

    (wip) test views/events/edit -> some error with parameters (@/assign) sent to rendering

--- spec/views/events/edit.html.haml_spec.rb
+++ spec/views/events/edit.html.haml_spec.rb
@@ -2,27 +2,28 @@ require 'rails_helper'
 
 describe "events/edit" do
   before(:each) do
-    @event_types= [FactoryBot.create(:event_type,
+    event_types= [FactoryBot.create(:event_type,
       :name => "ET Name",
       :description => "ET Descripcion",
       :recipients => "ET Recipients",
       :program => "ET Program"
       )]
-      @event = FactoryBot.create(:event,
-        :event_type => @event_types[0],
-        :place => "EvPlace",
-        :date => "2025-01-01",
-        :capacity => 12358,
-        :city => "Ev City"
-      )
-      assign(:event, @event)
-      @countries = [@event.country]
-      @trainers = [@event.trainer]
-      @timezones = ActiveSupport::TimeZone.all
-      @currencies = Money::Currency.table
-    end
+    @event = FactoryBot.create(:event,
+      :event_type => event_types[0],
+      :place => "EvPlace",
+      :date => "2025-01-01",
+      :capacity => 12358,
+      :city => "Ev City"
+    )
+    # assign(:event, @event)
+    # @countries = [@event.country]
+    # @trainers = [@event.trainer]
+    @timezones = ActiveSupport::TimeZone.all
+    @currencies = Money::Currency.table
+    @event_type_cancellation_policy = @event.event_type.cancellation_policy
+  end
   
-  it "renders the edit event type form" do
+  it "renders the edit event form" do
     pending "Not working. Fixed in Rails 4? https://github.com/rails/rails/issues/4401"
     render
 
