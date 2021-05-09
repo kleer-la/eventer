492c8c20673798166cbe35d9f2a9d58bd7d1f256
Juan Gabardini <jgabardini@computer.org>
2021-05-09 02:44:36 +0000

    remove views validation tests

--- spec/views/participants/certificate.pdf.prawn_spec.rb
+++ spec/views/participants/certificate.pdf.prawn_spec.rb
@@ -2,29 +2,6 @@
 require 'rails_helper'
 
 RSpec.describe "generate one certificate" do
-  it "abort when no page_size" do
-    render :template => "participants/certificate", format: :pdf
-    texts= PDF::Inspector::Text.analyze(rendered).strings
-    expect(texts[0]).to include 'LETTER'
-  end
-  it "abort when wrong verification code" do
-    assign(:page_size, 'LETTER')
-    render :template => "participants/certificate", format: :pdf
-    texts= PDF::Inspector::Text.analyze(rendered).strings
-    expect(texts[0]).to include 'código de verificación'
-  end
-  it "abort when wrong verification code" do
-    participant= FactoryBot.create(:participant)
-    assign(:page_size, 'LETTER')
-    assign(:verification_code, participant.verification_code)
-    assign(:certificate, ParticipantsHelper::Certificate.new(participant) )
-    assign(:participant, participant )
-
-    render :template => "participants/certificate", format: :pdf
-    texts= PDF::Inspector::Text.analyze(rendered).strings
-    expect(texts[0]).to include 'no estuvo presente'
-  end
-
   it "a correct one@participant." do
     participant= FactoryBot.create(:participant)
     participant.attend!
