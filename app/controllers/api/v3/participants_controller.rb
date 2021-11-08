f45f76879596ead9ce827278f5ca75f95c2a11f8
Juan Gabardini <jgabardini@computer.org>
2021-11-08 20:51:57 -0300

    interested in a event type

--- app/controllers/api/v3/participants_controller.rb
+++ app/controllers/api/v3/participants_controller.rb
@@ -1,17 +1,19 @@
 class Api::V3::ParticipantsController < ApplicationController
   def interest
     event_id= params[:event_id].to_i
-    if event_id > 0
-      _interest_event(event_id)
-      return
+    event_type_id= params[:event_type_id].to_i
+    if event_id == 0 and event_type_id > 0
+      event_id = Event.public_courses.where("event_type_id=?", event_type_id)[0].id
     end
+
+    _interest_event(event_id) if event_id > 0
   end
 
 private
 
   def _interest_event(id)
     event = Event.find(id)
-    result = event.interested_participant(
+    result = event&.interested_participant(
       params[:firstname], params[:lastname], 
       params[:email], params[:country_iso],
       params[:notes]
