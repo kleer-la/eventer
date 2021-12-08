593954997e799fa5977fd3fcadec141fca3194cf
Juan Gabardini <jgabardini@computer.org>
2021-12-08 19:12:34 -0300

    Article model spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before(:each) do
+    @article = FactoryBot.build(:article)
+  end
 
-# validates :title, presence: true
-# validates :description, presence: true
+  it "create a valid instance" do
+    expect(@article.valid?).to be true
+  end
+
+  it "title, body, description, slug are required" do
+     @article.title= ""
+     @article.body= ""
+     @article.description= ""
+     @article.slug= ""
+    expect(@article.valid?).to be false    
+    expect(@article.errors.count).to be 4
+  end
+end
