403b404ab1086b7a3a20b730f4620b37a52a6409
Juan Gabardini <jgabardini@computer.org>
2021-12-18 10:35:22 -0300

    slug can be changed or regenerated

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,9 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
     # validates :published, presence: true
-    validates :slug, presence: true
     validates :description, presence: true, length: { maximum: 160 }
 
+    def should_generate_new_friendly_id?
+      !slug.present?
+    end
 end
