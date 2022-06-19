0754265e50977e325bcc393f8069caa85005b5a2
Juan Gabardini <jgabardini@computer.org>
2022-06-19 15:59:33 -0300

    update money recaptcha will_paginate

--- app/controllers/api/v3/participants_controller.rb
+++ app/controllers/api/v3/participants_controller.rb
@@ -22,18 +22,18 @@ module Api
         params.permit!
         event_id = params[:event_id].to_i
         event_type_id = params[:event_type_id].to_i
-        p params.inspect
-        params.each do |single|
-          p single
-        end
+        # p params.inspect
+        # params.each do |single|
+        #   p single
+        # end
 
         if event_id.zero? && event_type_id.positive?
           event_id = Event.public_courses.where('event_type_id=?', event_type_id)[0]&.id
-          p '1---', event_id
+          # p '1---', event_id
         end
 
         result = _interest_event(event_id) if event_id.to_i.positive?
-        p '3---', result
+        # p '3---', result
         if result.present?
           render json: { error: result }, status: 400
         else
@@ -45,7 +45,7 @@ module Api
 
       def _interest_event(id)
         event = Event.find(id)
-        p '2---', event
+        # p '2---', event
         event&.interested_participant(
           params[:firstname], params[:lastname],
           params[:email], params[:country_iso],
