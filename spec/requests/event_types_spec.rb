a6528e14c73f625ec13a0a57ff2093ee9e26a758
Juan Gabardini <jgabardini@computer.org>
2022-08-29 17:54:37 -0300

    move testimonies endpoint to /api

--- spec/requests/event_types_spec.rb
+++ spec/requests/event_types_spec.rb
@@ -64,4 +64,21 @@ describe 'API Events GET /event_types' do
       expect(@json.count).to be > 0
     end
   end
+  context 'testimonies' do
+    it 'JSON?' do
+      event_type = FactoryBot.create(:event_type)
+      url = "/api/event_types/#{event_type.id}/testimonies.json"
+      get url
+      @json = JSON.parse(last_response.body)
+      expect(@json.count).to eq 0
+    end
+    it 'One testimony' do
+      participant = FactoryBot.create(:participant, testimony: 'Abradadabra')
+      url = "/api/event_types/#{participant.event.event_type.id}/testimonies.json"
+      get url
+      @json = JSON.parse(last_response.body)
+      expect(@json.count).to eq 1
+      expect(@json[0]['testimony']).to eq 'Abradadabra'
+    end
+  end
 end
