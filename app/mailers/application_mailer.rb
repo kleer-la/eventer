dfd58bbcebd775869f8b100becdbbae320f7824a
Juan Gabardini <jgabardini@computer.org>
2022-12-31 19:11:39 -0300

    filter contact_us mails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,9 +4,25 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'entrenamos@kleer.la'
   # layout 'mailer'
   def contact_us(name, email, context, subject, message)
-    mail(to: 'entrenamos@kleer.la',
+
+    unless self.class.valid_name?(name)
+      Log.log(:mail, :info,  
+        "Contact Us name:#{name}", 
+        "#{email} #{context}, message: #{message}"
+       )
+      return
+    end
+
+    mail(to: ENV['CONTACT_US_MAILTO'] || 'entrenamos@kleer.la',
       subject: "[Consulta] #{name} consulto sobre #{context} #{subject}",
       body: "#{message} \n#{'-'*10}\n#{context}\n#{name}<#{email}>"
     )
+  end
+
+  def self.valid_name?(name)
+    return false if name.to_s == ''
+    return false if !!/^[a-z]+[A-Z]/.match(name)
+    return false if name.length > 50
+    true
   end
 end
