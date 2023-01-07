8d7c585e172f6b62510ee8179b80ddfda38b8504
Juan Gabardini <jgabardini@computer.org>
2023-01-06 22:34:16 -0300

    remove secrets

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -11,5 +11,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Eventer::Application.config.secret_token = '024cd3285b43cb5d5e8f8afb7c2ef029d37fb3c8548b785100b05aaf1226bc43e1cadc981516cda04b61b4305062b862ca1f33cf7e3598d0696a3cb7c82b3ff9'
-Eventer::Application.config.secret_key_base = '5c10bab25671f7cb7c553d4db1be2549fec6ca1613f52254a69c6dfddf7ee12d2a079fc330a4d4d7eb8e53ee7dd79da39fea700b894f3c47b751363a48414487'
+Eventer::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] 
