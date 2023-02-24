755dea704525f28d1aaa7b9730ae14635b489be5
Juan Gabardini <jgabardini@computer.org>
2023-02-24 12:22:11 -0300

    try removing PostgreSQLAdapter monkeypatch

--- config/initializers/postgresql_adapter_monkeypatch.rb
+++ config/initializers/postgresql_adapter_monkeypatch.rb
@@ -1,21 +1,21 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'active_record/connection_adapters/postgresql_adapter'
+# require 'active_record/connection_adapters/postgresql_adapter'
 
-module ActiveRecord
-  module ConnectionAdapters
-    class PostgreSQLAdapter
-      def set_standard_conforming_strings
-        old = client_min_messages
-        self.client_min_messages = 'warning'
-        begin
-          execute('SET standard_conforming_strings = on', 'SCHEMA')
-        rescue StandardError
-          nil
-        end
-      ensure
-        self.client_min_messages = old
-      end
-    end
-  end
-end
+# module ActiveRecord
+#   module ConnectionAdapters
+#     class PostgreSQLAdapter
+#       def set_standard_conforming_strings
+#         old = client_min_messages
+#         self.client_min_messages = 'warning'
+#         begin
+#           execute('SET standard_conforming_strings = on', 'SCHEMA')
+#         rescue StandardError
+#           nil
+#         end
+#       ensure
+#         self.client_min_messages = old
+#       end
+#     end
+#   end
+# end
