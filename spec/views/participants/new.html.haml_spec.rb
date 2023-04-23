34a7271df3dae9d9a2cf3f61901029325c69b551
Juan Gabardini <jgabardini@computer.org>
2023-04-23 20:57:26 -0300

    try to fix participants/new spec... TODO added

--- spec/views/participants/new.html.haml_spec.rb
+++ spec/views/participants/new.html.haml_spec.rb
@@ -6,11 +6,14 @@ describe 'rendering new participant form' do
   pending 'Buy button' do
     @event= FactoryBot.create(:event)
     @participant = Participant.new
+    @influence_zones = InfluenceZone.sort_wo_republica
     @participant.event = @event
+    I18n.locale = :es
     @nakedform = false
-    assign(:influence_zones, [FactoryBot.create(:influence_zone)])
+    # @quantities = ParticipantsController.quantities_list
+    # TODO: move quantities_list to ParticipantsHelper
 
-    render template: 'patticipants/new' # , :action => 'new',:layout => "empty_layout"
+    render template: 'participants/new' # , :action => 'new',:layout => "empty_layout"
 
     expect(rendered).to match(/Juan Carlos/)
   end
