8b5049470ad489d416d700131ddef1ba4f6e552f
Juan Gabardini <jgabardini@computer.org>
2023-07-04 07:07:07 -0300

    alerts on event_tipes

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,15 +3,23 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: 'Dashboard'
 
   content title: 'Dashboard' do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
+
+    columns do
+      column do # left column
+        panel 'Alertas' do
+          cover_alert
+          brochure_alert
+          event_type_project_code_alert
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
     # columns do
     #   column do
     #     panel "Recent Posts" do
@@ -30,4 +38,33 @@ ActiveAdmin.register_page 'Dashboard' do
     #   end
     # end
   end # content
+end
+
+def brochure_alert
+  event_type_alert('Event Type sin brochure', 
+    EventType.where(include_in_catalog: true, deleted: false, brochure: [nil, ''])
+  )
+end
+
+def cover_alert
+  event_type_alert('Event Type sin brochure', 
+    EventType.where(include_in_catalog: true, deleted: false, cover: [nil, ''])
+  )
+end
+
+def event_type_project_code_alert
+  event_type_alert('Event Type sin código projecto', 
+    EventType.where(include_in_catalog: true, tag_name: [nil, ''])
+  )
+end
+
+def event_type_alert(message, error_list)
+  return if (error_list.count() == 0)
+
+  h4 "#{message} (#{error_list.count})"
+  ul do
+    error_list.each do |et|
+      li link_to(et.name, edit_event_type_path(et))
+    end
+  end
 end
