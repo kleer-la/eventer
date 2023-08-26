ee5f92f1ba537608793c74f5d5ad226f629edcfd
Juan Gabardini <jgabardini@computer.org>
2023-08-26 00:11:34 +0000

    change menu & eventType filters

--- app/admin/event_types.rb
+++ app/admin/event_types.rb
@@ -1,22 +1,20 @@
 ActiveAdmin.register EventType do
-  filter :name, filters: [:contains, :starts_with, :ends_with, :equals]
-  ActiveAdmin.register EventType do
-    filter :name, filters: [:contains, :starts_with, :ends_with, :equals]
+  filter :kleer_cert_seal_image_present, as: :boolean #, label: 'Cert Image is Empty'
+  filter :kleer_cert_seal_image_blank, as: :boolean #, label: 'Cert Image is Not Empty'
+  filter :kleer_cert_seal_image
 
-    EventType.attribute_names.each do |attribute|
-      filter attribute.to_sym, filters: [:contains, :starts_with, :ends_with, :equals]
-    end
+  EventType.attribute_names.each do |attribute|
+    filter attribute.to_sym
+  end
 
-    index do
-      column :name
-      column :lang
-      column :duration
-      column :deleted
-      column :noindex
-      actions defaults: false do |event_type|
-        link_to 'Edit', edit_event_type_path(event_type)
-      end
+  index do
+    column :name
+    column :lang
+    column :duration
+    column :deleted
+    column :noindex
+    actions defaults: false do |event_type|
+      link_to 'Edit', edit_event_type_path(event_type)
     end
   end
-  
 end
