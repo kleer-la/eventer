06ab0471841e202dc5439223047e1f84e366a4e0
Juan Gabardini <jgabardini@computer.org>
2023-11-07 17:56:17 +0000

    add Event to ActiveAdmin

---
+++ app/admin/events.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register Event do
+  Event.attribute_names.each do |attribute|
+    filter attribute.to_sym
+  end
+
+  index do
+    column :date
+    column "Tipo evento", :event_type do |event|
+      event.event_type&.name
+    end
+    column "Detalles", :event_type do |event|
+      "#{event.country.iso_code.downcase} #{event.city}"
+    end
+    column :draft
+    column :cancelled
+    column :is_sold_out
+    actions defaults: false do |event|
+      link_to 'Edit', edit_event_path(event)
+    end
+  end
+end
