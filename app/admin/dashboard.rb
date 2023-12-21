c5a83b2a91482799e6d1254575dbdf7d15b935e7
Juan Gabardini <jgabardini@computer.org>
2023-12-21 13:38:56 +0000

    activeadmin dashboard next courses

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: 'Dashboard'
 
@@ -16,6 +17,29 @@ ActiveAdmin.register_page 'Dashboard' do
           cover_alert
           brochure_alert
           event_type_project_code_alert
+        end
+      end
+      column do # left column
+        panel 'Próximos cursos' do
+          grouped_events.each do |period, events|
+            h3 period
+            table_for events, class: 'no-table-headings' do
+              column nil do |event|
+                "#{event.human_date}"
+              end
+              column nil do |event|
+                human_hours = event.start_time.strftime( "%H:%Mhs." )
+                trainers = event.trainers.pluck(:name).join(',')
+                link_to "#{event.event_type.name} - #{event.date} | #{event.city} | #{human_hours} | #{event.place} | #{trainers}",
+                        event_participants_path(event)
+              end
+              column nil do |event|
+                if event.registration_link == ""
+                  "#{(event.completion*100).round}% (#{event.confirmed_quantity}/#{event.capacity}) "
+                end
+              end
+            end
+          end
         end
       end
     end
@@ -67,4 +91,21 @@ def event_type_alert(message, error_list)
       li link_to(et.name, edit_event_type_path(et))
     end
   end
+end
+
+def dweek(d)
+   (d.year * 100 + d.cweek) - (Date.today.year*100+Date.today.cweek)
+end
+
+def grouped_events
+  events = Event.public_and_visible.order('date').select do |ev|
+    !ev.event_type.nil? && ev.registration_link == ''
+  end
+
+  {
+    'Up to 2 Weeks' => events.select{ |ev| dweek(ev.date)<=2 },
+    '2-4 Weeks' => events.select{ |ev| dweek(ev.date).between?(3,4) },
+    '4-6 Weeks' => events.select{ |ev| dweek(ev.date).between?(5,6) },
+    '6+ Weeks' => events.select{ |ev| dweek(ev.date)>6 }
+  }
 end
