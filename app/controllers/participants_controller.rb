0e795864108b208293bb4a724370a0b872697d50
Juan Gabardini <jgabardini@computer.org>
2024-02-01 16:50:42 +0000

    rollback to delayed job for xero communication
    undefined

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -187,8 +187,8 @@ class ParticipantsController < ApplicationController
   end
 
   def create_mails
-    # EventMailer.delay.welcome_new_event_participant(@participant) if @event.should_welcome_email
-    EventMailer.welcome_new_event_participant(@participant) if @event.should_welcome_email
+    EventMailer.delay.welcome_new_event_participant(@participant) if @event.should_welcome_email
+    # EventMailer.welcome_new_event_participant(@participant) if @event.should_welcome_email
 
     edit_registration_link = "http://#{request.host}/events/#{@participant.event.id}/participants/#{@participant.id}/edit"
     EventMailer.delay.alert_event_monitor(@participant, edit_registration_link)
