40bc4a17d34529bd8a90fe8122b2978845476a51
Juan Gabardini <jgabardini@computer.org>
2024-02-01 16:51:06 +0000

    access to token generation

--- app/admin/oauth_tokens.rb
+++ app/admin/oauth_tokens.rb
@@ -5,4 +5,13 @@ ActiveAdmin.register OauthToken do
   filter :created_at
 
   actions :index, :show
+
+  collection_action :new_oauth_token, method: :get do
+    xero_client = XeroClientService.build_client
+    redirect_to xero_client.authorization_url, allow_other_host: true
+  end
+
+  action_item :new_oauth_token, only: :index do
+    link_to 'New Oauth Token', new_oauth_tokens_path
+  end
 end
