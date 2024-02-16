10ad1a9d75ece550a8c4a3d2fb069917db5411a0
Juan Gabardini <jgabardini@computer.org>
2024-02-16 14:11:47 +0000

    Log also to text log file (dev / test)

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,12 +3,9 @@ class Log < ApplicationRecord
   enum level: %i[info warn error]
 
   def self.log(area, level, message, details = nil)
-    l = Log.new
-    l.area = area
-    l.level = level
-    l.message = message
-    l.details = details
-    l.save!
+    l = Log.create!(area: area, level: level, message: message, details: details)
+
+    Rails.logger.debug l.to_s if Rails.env.development? || Rails.env.test?
   end
   def self.ransackable_attributes(auth_object = nil)
     ["area", "created_at", "details", "id", "id_value", "level", "message", "updated_at"]
