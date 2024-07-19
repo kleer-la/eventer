83cfb8be83d22d542c5caf8b530f2e9aaf359978
Juan Gabardini <jgabardini@computer.org>
2024-07-19 21:47:09 +0000

    add rackup - trying to get to h2

--- config/puma.rb
+++ config/puma.rb
@@ -35,6 +35,6 @@ workers ENV.fetch('WEB_CONCURRENCY') { 2 }
 # process behavior so workers use less memory.
 #
 preload_app!
-
+rackup DefaultRackup
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
