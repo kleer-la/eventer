22ee116e0b9136950aaa958fa1bc54a2bd04063f
Juan Gabardini <jgabardini@computer.org>
2024-08-27 22:14:41 +0000

    make recommended content index more robust

--- app/admin/recommended_content.rb
+++ app/admin/recommended_content.rb
@@ -6,18 +6,19 @@ ActiveAdmin.register RecommendedContent do
     id_column
     column :source do |rc|
       link_to "#{rc.source_type} - #{rc.source.title}", edit_polymorphic_path([:admin, rc.source])
-    end
-    column :title do |rc|
-      link_to rc.source.title, edit_polymorphic_path([:admin, rc.source])
+    rescue NoMethodError, ActionController::UrlGenerationError
+      "#{rc.source_type} - #{rc.source.title}"
     end
     column :target do |rc|
+      link_to "#{rc.target_type} - #{rc.target.title}", edit_polymorphic_path([:admin, rc.target])
+    rescue NoMethodError, ActionController::UrlGenerationError
       "#{rc.target_type} - #{rc.target.title}"
     end
     column :relevance_order
   end
 
-  filter :source_type, as: :select, collection: %w[Service Article EventType]
-  filter :target_type, as: :select, collection: %w[Service Article EventType]
+  filter :source_type, as: :select, collection: %w[Service Article EventType Page]
+  filter :target_type, as: :select, collection: %w[Service Article EventType Page]
   filter :relevance_order
 
   controller do
