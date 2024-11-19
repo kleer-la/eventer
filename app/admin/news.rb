9666aca2b1d9a58d4a8823014559f80a4d24dab4
Juan Gabardini <jgabardini@computer.org>
2024-11-19 13:26:44 +0000

    enable news edit

---
+++ app/admin/news.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register News do
+  menu parent: 'We Publish'
+  permit_params :lang, :title, :where, :description, :url, :img, :video, :audio, :event_date
+
+  index do
+    selectable_column
+    id_column
+    column :lang
+    column :title
+    column :where
+    column :event_date
+    column :created_at
+    actions
+  end
+
+  filter :lang
+  filter :title
+  filter :where
+  filter :event_date
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :lang, as: :select, collection: News.langs
+      f.input :title
+      f.input :where
+      f.input :description
+      f.input :url
+      f.input :img
+      f.input :video
+      f.input :audio
+      f.input :event_date, as: :datepicker
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :lang
+      row :title
+      row :where
+      row :description
+      row :url do |news|
+        if news.url.present?
+          div link_to news.url, news.url, target: '_blank'
+          div iframe src: news.url, width: '100%', height: '400px'
+        end
+      end
+      row :img do |news|
+        if news.img.present?
+          img_url = news.img.start_with?('/') ? "https://www.kleer.la#{news.img}" : news.img
+          div link_to img_url, news.img, target: '_blank'
+          div image_tag img_url, style: 'max-width: 500px'
+        end
+      end
+      row :video do |news|
+        if news.video.present?
+          div link_to news.video, news.video, target: '_blank'
+          div iframe src: news.video, width: '100%', height: '400px'
+        end
+      end
+      row :audio do |news|
+        if news.audio.present?
+          div link_to news.audio, news.audio, target: '_blank'
+          div audio controls: true do
+            source src: news.audio
+          end
+        end
+      end
+      row :event_date
+      row :created_at
+      row :updated_at
+    end
+  end
+end
--- app/models/news.rb
+++ app/models/news.rb
@@ -3,4 +3,9 @@
 class News < ApplicationRecord
   has_and_belongs_to_many :trainers
   enum lang: %i[es en]
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[audio created_at description event_date id id_value img lang title updated_at url
+       video where]
+  end
 end
