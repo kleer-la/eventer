339c7584767640b4f070e15ef1513dea803f6522
Juan Gabardini <jgabardini@computer.org>
2024-11-20 20:51:37 +0000

    set active_job to use delayed job

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "eventer_production"
+  config.active_job.queue_adapter = :delayed_job
+  config.active_job.queue_name_prefix = 'eventer_production'
 
   config.action_mailer.perform_caching = false
 
