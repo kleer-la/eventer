976812a04beae862cffc491079df8bddcd016db1
Juan Gabardini <jgabardini@computer.org>
2024-11-23 23:32:57 +0000

    make ActiveAdmin defaut

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root to: 'dashboard#index'
+  # root to: 'dashboard#index'
+  root to: 'admin/dashboard#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
