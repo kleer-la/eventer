1579b19a9279a93df36e05e27d528f37ddc3c703
Juan Gabardini <jgabardini@computer.org>
2024-12-09 17:58:10 +0000

    better edit of participant

--- app/admin/participants.rb
+++ app/admin/participants.rb
@@ -15,7 +15,11 @@ ActiveAdmin.register Participant do
 
   # Permit params for editing
   permit_params :email, :fname, :lname, :phone, :event_id, :status, :notes,
-                :influence_zone_id, :quantity, :referer_code, :address
+                :influence_zone_id, :quantity, :referer_code, :address,
+                :company_name, :id_number, :invoice_id, :xero_invoice_number,
+                :is_payed, :selected, :testimony, :photo_url, :profile_url,
+                :accept_terms, :promoter_score, :event_rating, :trainer_rating,
+                :trainer2_rating, :campaign_source, :campaign
 
   # Index page customization
   index do
@@ -109,6 +113,141 @@ ActiveAdmin.register Participant do
 
     def icon_span(icon_name, text)
       "<span style='white-space:nowrap;'><i class='icon-#{icon_name}'></i> #{text}</span>".html_safe
+    end
+  end
+
+  form do |f|
+    tabs do
+      tab 'General' do
+        f.inputs do
+          f.input :fname
+          f.input :lname
+          f.input :email
+          f.input :phone
+          f.input :address
+          f.input :company_name
+          f.input :id_number
+          f.input :status, as: :select, collection: STATUS_LIST
+          f.input :event, collection: Event.all.sort_by(&:unique_name),
+                         include_blank: 'Selecciona uno...',
+                         input_html: { style: 'width:500px' }
+          f.input :influence_zone, collection: InfluenceZone.all,
+                                 include_blank: 'Tu lugar más próximo ...',
+                                 hint: proc { |p| p.object.influence_zone&.tag_name || 'N/A' }
+          f.input :quantity
+          f.input :notes, input_html: { rows: 10 }
+          f.input :referer_code
+          f.input :campaign_source
+          f.input :campaign
+        end
+      end
+
+      tab 'Payment' do
+        f.inputs do
+          f.input :invoice_id, placeholder: '3cae5630-2189-4489-8464-b415e201da7d'
+          f.input :xero_invoice_number, placeholder: 'FC-00000'
+          f.input :is_payed
+        end
+      end
+
+      tab 'Testimony' do
+        f.inputs do
+          f.input :selected
+          f.input :testimony
+          f.input :photo_url
+          div class: 'photo-preview' do
+            if f.object.photo_url.present?
+              image_tag f.object.photo_url, style: 'max-width: 300px; margin-top: 10px;'
+            end
+          end
+          f.input :profile_url
+          f.input :accept_terms
+        end
+      end
+
+      tab 'NPS' do
+        f.inputs do
+          f.input :promoter_score
+          f.input :event_rating
+          f.input :trainer_rating
+          f.input :trainer2_rating
+        end
+      end
+    end
+    f.actions
+  end
+
+  # Add STATUS_LIST constant at the top of the file
+  STATUS_LIST = [
+    ['Nuevo', 'N'],
+    ['Contactado', 'T'],
+    ['Confirmado', 'C'],
+    ['Presente', 'A'],
+    ['Certificado', 'K'],
+    ['Cancelado', 'X'],
+    ['Pospuesto', 'D']
+  ].freeze
+
+  show do
+    tabs do
+      tab 'General' do
+        attributes_table do
+          row :fname
+          row :lname
+          row :email
+          row :phone
+          row :address
+          row :company_name
+          row :id_number
+          row :status do |participant|
+            status_tag(
+              status_text(participant.status),
+              class: participant_status_class(participant.status)
+            )
+          end
+          row :event
+          row :influence_zone
+          row :quantity
+          row :notes
+          row :referer_code
+          row :campaign_source
+          row :campaign
+        end
+      end
+
+      tab 'Payment' do
+        attributes_table do
+          row :invoice_id
+          row :xero_invoice_number
+          row :is_payed
+        end
+      end
+
+      tab 'Testimony' do
+        attributes_table do
+          row :selected
+          row :testimony
+          row :photo_url
+          row :photo do |participant|
+            if participant.photo_url.present?
+              div class: 'photo-preview' do
+                image_tag participant.photo_url, style: 'max-width: 300px;'
+              end
+            end
+          end
+          row :profile_url
+          row :accept_terms
+        end
+      end
+
+      tab 'NPS' do
+        attributes_table do
+          row :promoter_score
+          row :event_rating
+          row :trainer_rating
+          row :trainer2_rating
+        end
+      end
     end
   end
 end
