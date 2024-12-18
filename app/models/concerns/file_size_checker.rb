9ccc4d88d82cd93dd8797835dab626ced0dcb123
Juan Gabardini <jgabardini@computer.org>
2024-12-18 14:07:12 +0000

    handle locals

--- app/models/concerns/file_size_checker.rb
+++ app/models/concerns/file_size_checker.rb
@@ -4,20 +4,29 @@ module FileSizeChecker
   extend ActiveSupport::Concern
   require 'cgi'
 
+  BASE_URL = 'https://www.kleer.la'
+
   def get_remote_file_size(url)
     return 0 if url.blank?
 
-    begin
-      # Split the URL into base and path
-      uri = URI(url.gsub(' ', '%20')) # Simple space replacement
+    full_url = url.start_with?('http://', 'https://') ? url : "#{BASE_URL}#{url}"
 
+    begin
+      uri = URI(full_url.gsub(' ', '%20'))
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = (uri.scheme == 'https')
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE # Be careful with this in production
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      response = http.request_head(uri.path)
+      # Create request with full path and headers
+      request = Net::HTTP::Head.new(uri.request_uri)
+      request['User-Agent'] = "Ruby/#{RUBY_VERSION}"
+      request['Accept'] = '*/*'
+      request['Accept-Encoding'] = 'identity' # Request uncompressed content
+
+      response = http.request(request)
+
       size = response['content-length'].to_i
-      Rails.logger.info "Size for #{url}: #{size} bytes"
+      Rails.logger.info "Size for #{full_url}: #{size} bytes"
       size
     rescue StandardError => e
       Rails.logger.error "Error checking file size for #{url}: #{e.message}"
