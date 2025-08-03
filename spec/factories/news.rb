61fdaf13cbdaa776c13224a55713207008504e1e
Juan Gabardini <jgabardini@computer.org>
2025-08-03 19:53:39 +0000

    Add news factory and comprehensive model tests

---
+++ spec/factories/news.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :news do
+    lang { 'es' }
+    title { 'Breaking News: Agile Training Revolution' }
+    where { 'Buenos Aires, Argentina' }
+    description { 'This is an exciting development in the world of agile training and coaching.' }
+    url { 'https://example.com/news/agile-training-revolution' }
+    img { 'https://example.com/images/news-image.jpg' }
+    event_date { 1.week.from_now.to_date }
+
+    trait :english do
+      lang { 'en' }
+      title { 'Breaking News: Agile Training Revolution' }
+      where { 'Buenos Aires, Argentina' }
+      description { 'This is an exciting development in the world of agile training and coaching.' }
+    end
+
+    trait :spanish do
+      lang { 'es' }
+      title { 'Últimas Noticias: Revolución en Entrenamiento Ágil' }
+      where { 'Buenos Aires, Argentina' }
+      description { 'Este es un desarrollo emocionante en el mundo del entrenamiento y coaching ágil.' }
+    end
+
+    trait :with_media do
+      video { 'https://youtube.com/watch?v=example' }
+      audio { 'https://podcast.example.com/episode-1' }
+    end
+
+    trait :with_trainers do
+      after(:create) do |news|
+        news.trainers << create(:trainer)
+        news.trainers << create(:trainer2)
+      end
+    end
+
+    trait :past_event do
+      event_date { 2.weeks.ago.to_date }
+    end
+
+    trait :future_event do
+      event_date { 1.month.from_now.to_date }
+    end
+  end
+end
--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -3,5 +3,247 @@
 require 'rails_helper'
 
 RSpec.describe News, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'factory' do
+    it 'creates a valid news item' do
+      news = build(:news)
+      expect(news).to be_valid
+    end
+  end
+
+  describe 'attributes' do
+    let(:news) { build(:news) }
+
+    it 'has required attributes' do
+      expect(news).to respond_to(:lang)
+      expect(news).to respond_to(:title)
+      expect(news).to respond_to(:where)
+      expect(news).to respond_to(:description)
+      expect(news).to respond_to(:url)
+      expect(news).to respond_to(:img)
+      expect(news).to respond_to(:video)
+      expect(news).to respond_to(:audio)
+      expect(news).to respond_to(:event_date)
+    end
+
+    it 'stores attributes correctly' do
+      news.title = 'Test News Title'
+      news.where = 'Test Location'
+      news.description = 'Test description'
+      news.url = 'https://test.com'
+
+      expect(news.title).to eq('Test News Title')
+      expect(news.where).to eq('Test Location')
+      expect(news.description).to eq('Test description')
+      expect(news.url).to eq('https://test.com')
+    end
+  end
+
+  describe 'language enum' do
+    it 'defines language enum correctly' do
+      expect(News.langs).to eq({ 'es' => 0, 'en' => 1 })
+    end
+
+    it 'allows setting language as string' do
+      news = build(:news, lang: 'es')
+      expect(news.lang).to eq('es')
+      expect(news.es?).to be true
+      expect(news.en?).to be false
+    end
+
+    it 'allows setting language as integer' do
+      news = build(:news, lang: 1)
+      expect(news.lang).to eq('en')
+      expect(news.en?).to be true
+      expect(news.es?).to be false
+    end
+
+    it 'provides language query methods' do
+      spanish_news = create(:news, :spanish)
+      english_news = create(:news, :english)
+
+      expect(spanish_news.es?).to be true
+      expect(spanish_news.en?).to be false
+      expect(english_news.en?).to be true
+      expect(english_news.es?).to be false
+    end
+
+    it 'can filter by language' do
+      spanish_news = create(:news, :spanish)
+      english_news = create(:news, :english)
+
+      spanish_items = News.where(lang: 'es')
+      english_items = News.where(lang: 'en')
+
+      expect(spanish_items).to include(spanish_news)
+      expect(spanish_items).not_to include(english_news)
+      expect(english_items).to include(english_news)
+      expect(english_items).not_to include(spanish_news)
+    end
+  end
+
+  describe 'associations' do
+    it 'has and belongs to many trainers' do
+      news = create(:news)
+      trainer1 = create(:trainer)
+      trainer2 = create(:trainer2)
+
+      news.trainers << trainer1
+      news.trainers << trainer2
+
+      expect(news.trainers).to include(trainer1, trainer2)
+      expect(trainer1.news).to include(news)
+      expect(trainer2.news).to include(news)
+    end
+
+    it 'works with factory trait for trainers' do
+      news = create(:news, :with_trainers)
+
+      expect(news.trainers.count).to eq(2)
+      expect(news.trainers.first).to be_a(Trainer)
+    end
+
+    it 'can remove trainers' do
+      news = create(:news, :with_trainers)
+      initial_count = news.trainers.count
+      trainer_to_remove = news.trainers.first
+
+      news.trainers.delete(trainer_to_remove)
+
+      expect(news.trainers.count).to eq(initial_count - 1)
+      expect(news.trainers).not_to include(trainer_to_remove)
+    end
+  end
+
+  describe 'ImageReference concern' do
+    it 'includes ImageReference concern' do
+      expect(News.included_modules).to include(ImageReference)
+    end
+
+    it 'references images in url field' do
+      news = create(:news, url: 'https://example.com/article.html')
+      expect(news.url).to eq('https://example.com/article.html')
+    end
+
+    it 'references images in img field' do
+      news = create(:news, img: 'https://example.com/image.jpg')
+      expect(news.img).to eq('https://example.com/image.jpg')
+    end
+
+    it 'references images in text fields like description' do
+      description_with_image = 'Check out this image: https://example.com/embedded.jpg'
+      news = create(:news, description: description_with_image)
+
+      expect(news.description).to include('https://example.com/embedded.jpg')
+    end
+  end
+
+  describe 'media fields' do
+    it 'stores video URLs' do
+      news = create(:news, video: 'https://youtube.com/watch?v=example')
+      expect(news.video).to eq('https://youtube.com/watch?v=example')
+    end
+
+    it 'stores audio URLs' do
+      news = create(:news, audio: 'https://podcast.example.com/episode-1')
+      expect(news.audio).to eq('https://podcast.example.com/episode-1')
+    end
+
+    it 'works with media trait' do
+      news = create(:news, :with_media)
+
+      expect(news.video).to be_present
+      expect(news.audio).to be_present
+      expect(news.video).to include('youtube.com')
+      expect(news.audio).to include('podcast.example.com')
+    end
+  end
+
+  describe 'event date handling' do
+    it 'stores event dates correctly' do
+      event_date = 1.week.from_now.to_date
+      news = create(:news, event_date: event_date)
+
+      expect(news.event_date).to eq(event_date)
+    end
+
+    it 'can filter by date ranges' do
+      past_news = create(:news, :past_event)
+      future_news = create(:news, :future_event)
+
+      past_items = News.where('event_date < ?', Date.current)
+      future_items = News.where('event_date > ?', Date.current)
+
+      expect(past_items).to include(past_news)
+      expect(past_items).not_to include(future_news)
+      expect(future_items).to include(future_news)
+      expect(future_items).not_to include(past_news)
+    end
+  end
+
+  describe 'Ransack functionality' do
+    it 'returns expected searchable attributes' do
+      expected_attributes = %w[audio created_at description event_date id id_value img lang
+                               title updated_at url video where]
+
+      expect(News.ransackable_attributes).to match_array(expected_attributes)
+    end
+
+    it 'can search by title using Ransack' do
+      agile_news = create(:news, title: 'Agile Training Workshop')
+      scrum_news = create(:news, title: 'Scrum Master Certification')
+
+      # Simulate Ransack search
+      agile_results = News.where('title LIKE ?', '%Agile%')
+      scrum_results = News.where('title LIKE ?', '%Scrum%')
+
+      expect(agile_results).to include(agile_news)
+      expect(agile_results).not_to include(scrum_news)
+      expect(scrum_results).to include(scrum_news)
+      expect(scrum_results).not_to include(agile_news)
+    end
+  end
+
+  describe 'data queries and scopes' do
+    before do
+      @spanish_news = create(:news, :spanish, title: 'Noticias en Español')
+      @english_news = create(:news, :english, title: 'English News')
+      @news_with_video = create(:news, video: 'https://youtube.com/example')
+      @news_without_video = create(:news, video: nil)
+    end
+
+    it 'can filter by language' do
+      spanish_items = News.es
+      english_items = News.en
+
+      expect(spanish_items).to include(@spanish_news)
+      expect(spanish_items).not_to include(@english_news)
+      expect(english_items).to include(@english_news)
+      expect(english_items).not_to include(@spanish_news)
+    end
+
+    it 'can filter by presence of media' do
+      with_video = News.where.not(video: [nil, ''])
+      without_video = News.where(video: [nil, ''])
+
+      expect(with_video).to include(@news_with_video)
+      expect(with_video).not_to include(@news_without_video)
+      expect(without_video).to include(@news_without_video)
+    end
+
+    it 'can order by event date' do
+      old_news = create(:news, event_date: 1.month.ago, title: 'Old News')
+      recent_news = create(:news, event_date: 1.day.ago, title: 'Recent News')
+      future_news = create(:news, event_date: 1.week.from_now, title: 'Future News')
+
+      # Query only these three specific news items
+      test_news_ids = [old_news.id, recent_news.id, future_news.id]
+      chronological = News.where(id: test_news_ids).order(:event_date)
+      reverse_chronological = News.where(id: test_news_ids).order(event_date: :desc)
+
+      expect(chronological.first).to eq(old_news)
+      expect(chronological.last).to eq(future_news)
+      expect(reverse_chronological.first).to eq(future_news)
+      expect(reverse_chronological.last).to eq(old_news)
+    end
+  end
 end
