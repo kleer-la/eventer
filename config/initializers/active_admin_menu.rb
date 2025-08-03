4ed01d576003275044f48fe7cf66e595b77ead62
Juan Gabardini <jgabardini@computer.org>
2025-08-03 19:11:25 +0000

    Remove 'Old Version' menu item from ActiveAdmin menu

--- config/initializers/active_admin_menu.rb
+++ config/initializers/active_admin_menu.rb
@@ -5,7 +5,6 @@ ActiveAdmin.setup do |config|
       menu.add label: 'Services Mgnt', priority: 4
       menu.add label: 'We Publish', priority: 8
       menu.add label: 'Others', priority: 10
-      menu.add label: 'Old Version', url: '/dashboard', priority: 100
     end
   end
 end
